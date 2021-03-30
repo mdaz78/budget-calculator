60e615394cd5db1ae84adf3cf23b3458097b59f1
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-03-30 12:31:22 +0530

    feat: Add Expense List
    - Create Expense Component
    - Create ExpenseList Component
    - Toggle Amount in Expense Card

---
+++ src/Components/Expense.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let name;
+  export let amount;
+
+  let displayAmount = false;
+
+  function toggleDisplayAmount() {
+    displayAmount = !displayAmount;
+  }
+</script>
+
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name}
+      <button class="amount-btn" on:click={toggleDisplayAmount}>
+        <i class="fas fa-caret-down" />
+      </button>
+    </h2>
+    {#if displayAmount}
+      <h4>amount : ${amount}</h4>
+    {/if}
+  </div>
+  <div class="expense-buttons">
+    <button class="expense-btn edit-btn"> <i class="fas fa-pen" /> </button>
+    <button class="expense-btn delete-btn"> <i class="fas fa-trash" /> </button>
+  </div>
+</article>
